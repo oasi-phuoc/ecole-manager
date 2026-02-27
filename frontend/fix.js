2a7a8c1632540076c70feeaac574b651ef416ea3
Thuy-96 <tthuy.van@outlook.com>
2026-02-27 16:30:06 +0100

    Fix build command Vercel

--- frontend/fix.js
+++ frontend/fix.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 
-// Créer vercel.json dans le dossier frontend
 fs.writeFileSync('./vercel.json', `
 {
-  "buildCommand": "DISABLE_ESLINT_PLUGIN=true npm run build",
-  "outputDirectory": "build"
+  "buildCommand": "npm install && npm run build",
+  "outputDirectory": "build",
+  "installCommand": "npm install"
 }
 `.trim());
 
-// Mettre à jour .env
-fs.writeFileSync('./.env', `DISABLE_ESLINT_PLUGIN=true\nGENERATE_SOURCEMAP=false`);
-
-console.log('OK !');
+console.log('vercel.json mis a jour !');
